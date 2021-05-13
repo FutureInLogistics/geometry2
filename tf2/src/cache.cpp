ff721a26e6580be0340c8294a8fe0502905a2f7e
Markus Weber <weber@filics.eu>
2021-05-13 13:02:21 +0200

    Overwrite tf on repeated data
    * Apply this commit: https://github.com/ros/geometry2/pull/475/commits/f0cb53da83849b38a26d619acf1c1a7f06df9ca2

--- tf2/src/cache.cpp
+++ tf2/src/cache.cpp
@@ -273,16 +273,17 @@ bool TimeCache::insertData(const TransformStorage& new_data, std::string* error_
       break;
     storage_it++;
   }
+  // Allow to replace TF with the same timestamp
   if (storage_it != storage_.end() && storage_it->stamp_ == new_data.stamp_)
   {
-    return true;
+    *storage_it = new_data;
   }
   else
   {
     storage_.insert(storage_it, new_data);
+    pruneList();
   }
 
-  pruneList();
   return true;
 }
 
